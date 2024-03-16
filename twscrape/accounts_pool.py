cc8d622a0d00aa4413c961db0c63580b8b139317
DaeHee Lee <lee111dae11@proton.me>
2024-03-17 02:31:29 +0900

    logging

--- twscrape/accounts_pool.py
+++ twscrape/accounts_pool.py
@@ -73,7 +73,7 @@ class AccountsPool:
         qs = "SELECT * FROM accounts WHERE username = :username"
         rs = await fetchone(self._db_file, qs, {"username": username})
         if rs:
-            logger.warning(f"Account {username} already exists")
+            # logger.warning(f"Account {username} already exists")
             return
 
         account = Account(
